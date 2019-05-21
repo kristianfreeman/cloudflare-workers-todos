b30c43d3056a5a2abeaa0e5af862aec6e6c7b3c2
Kristian Freeman <kristian@kristianfreeman.com>
2019-05-21 13:50:49 -0500

    Use cache.put instead of fetch

--- workers/todos.js
+++ workers/todos.js
@@ -102,29 +102,13 @@ async function getTodos(request) {
   })
 }
 
-const putInCache = (cacheKey, body) => {
-  const accountId = '$accountId'
-  const namespaceId = '$namespaceId'
-  return fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/values/${cacheKey}`,
-    {
-      method: 'PUT',
-      body,
-      headers: {
-        'X-Auth-Email': '$cloudflareEmail',
-        'X-Auth-Key': '$cloudflareApiKey',
-      },
-    },
-  )
-}
-
 async function updateTodos(request) {
   const body = await request.text()
   const ip = request.headers.get('CF-Connecting-IP')
   const cacheKey = `data-${ip}`
   try {
     JSON.parse(body)
-    await putInCache(cacheKey, body)
+    await setCache(cacheKey, body)
     return new Response(body, { status: 200 })
   } catch (err) {
     return new Response(err, { status: 500 })
